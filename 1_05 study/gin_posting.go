a8fa02888c0151efe8f59a18be6b10cb53541a7a
{Min chan} <{mandoo0402@konkuk.ac.kr}>
2023-01-06 15:34:19 +0900

    added like function.

--- 1_05 study/gin_posting.go
+++ 1_05 study/gin_posting.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"strconv"
 )
 
@@ -13,6 +13,7 @@ type Post struct {
 	ID      int    `json:"id"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
+	Like    int    `json:"like"`
 }
 
 // Posts is a slice of Post that represents the post board.
@@ -41,7 +42,7 @@ func main() {
 
 		defer db.Close()
 		// Insert the new post into the database
-		result, err := db.Exec("INSERT INTO postsave (Title, Content) VALUES (?, ?)", post.Title, post.Content)
+		result, err := db.Exec("INSERT INTO liketable (Title, Content) VALUES (?, ?)", post.Title, post.Content)
 		if err != nil {
 			c.JSON(500, gin.H{"error": err.Error()})
 			return
@@ -66,20 +67,56 @@ func main() {
 
 	// Inquire about a specific post by ID
 	router.GET("/posts/:id", func(c *gin.Context) {
+		db, err := sql.Open("mysql", "root:polarbear21*@tcp(localhost:3306)/winter")
+		defer db.Close()
+
+		// Check for database connection errors
+		if err != nil {
+			c.JSON(500, gin.H{"error": "failed to connect to database"})
+			return
+		}
+
+		// Get the ID parameter from the request
 		idParam := c.Param("id")
-		fmt.Println("Received ID:", idParam)
+
+		// Convert the ID parameter to an integer
 		id, err := strconv.Atoi(idParam)
 		if err != nil {
 			c.JSON(400, gin.H{"error": "invalid ID"})
 			return
 		}
-		for _, post := range Posts {
-			if post.ID == id {
-				c.JSON(200, post)
+
+		// Query the database for a post with the matching ID
+		var post Post
+		row := db.QueryRow("SELECT * FROM liketable WHERE ID=?", id)
+		err = row.Scan(&post.ID, &post.Title, &post.Content, &post.Like)
+
+		// Check for query errors
+		if err != nil {
+			if err == sql.ErrNoRows {
+				c.JSON(404, gin.H{"error": "post not found"})
 				return
 			}
+			c.JSON(500, gin.H{"error": "failed to retrieve post"})
+			return
 		}
-		c.JSON(404, gin.H{"error": "post not found"})
+
+		// Increment the like count for the post and update the database
+		post.Like++
+		_, err = db.Exec("UPDATE liketable SET `Like` = ? WHERE ID =?", post.Like, post.ID)
+
+		if err != nil {
+			log.Println("Error:", err)
+		}
+
+		// Check for update errors
+		if err != nil {
+			c.JSON(500, gin.H{"error": "failed to update post"})
+			return
+		}
+
+		// Return the post to the client
+		c.JSON(200, post)
 	})
 
 	router.Run()
